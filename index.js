f5739fd242cada2c331d858254528f179bf25e7f
User <tonmoy.sufian01@gmail.com>
2025-11-15 02:03:25 +0600

    Initial backend commit

--- index.js
+++ index.js
@@ -7,7 +7,14 @@ const fs = require("fs");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
-app.use(cors());
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "*",
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
 // Serve static files for uploaded evidence
